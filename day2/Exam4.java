252c6abb9a420936286fcaa863cff007cf2eab59
Huihuing <3826com@gmail.com>
2024-03-05 17:45:25 +0900

    JAVA Practice

--- day2/Exam4.java
+++ day2/Exam4.java
@@ -3,7 +3,7 @@ package day2;
 public class Exam4 {
     public static void main(String[] args) {
         // 배열 x
-         String[] star = {"*", "**", "***", "****", "*****", "******", "*******"};
+        /* String[] star = {"*", "**", "***", "****", "*****", "******", "*******"};
 
          for (int a = 0; a<3; a++) {
              System.out.print(star[a]);
@@ -17,17 +17,28 @@ public class Exam4 {
         System.out.println("\n");
         for (int c = 0; c<7; c++) {
             System.out.println(star[c]);
-        }
+        }*/
 
-        int height1 = 3;
-        System.out.println("높이 : [" + height1 + "]\n");
+        /*int height1 = 4;
+        //System.out.println("높이 : [" + height1 + "]\n");
         for (int i = 0; i < height1; i++) {
-            for (int j = 0; j < height1 - i - 1; j++) {
+            for (int j = 0; j < height1 - i; j++) {
                 System.out.print(" ");
             }
             for (int k = 0; k <= i; k++) {
                 System.out.print("*");
             }
+            System.out.println();
+        }
+*/
+        int down = 3;
+        for (int a=0; a<down; a++) {
+            for (int b=0; b <= a; b++) {
+                System.out.print("*");
+            }
+            /*for (int x=0; x < down; x++) {
+                System.out.print("+");
+            }*/
             System.out.println();
         }
 
